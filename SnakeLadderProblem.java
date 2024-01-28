ad461cfbb2bffb479f8fc8aa492298470624a5ef
Aachal131 <83298583+Aachal131@users.noreply.github.com>
2024-01-28 18:43:30 +0530

    Add files via upload

--- SnakeLadderProblem.java
+++ SnakeLadderProblem.java
@@ -1,7 +1,14 @@
 package com.pratice;
 
+import java.util.Random;
+
 public class SnakeLadderProblem {
     public static void main (String args[]){
         System.out.println("Welcome to SnakeLadderProblem");
+        int startPosition = 0;
+    }
+    private static int rollDice() {
+        Random random = new Random();
+        return random.nextInt(6) + 1;
     }
 }
