55560006ca3ed3f24fe1385bf924c04d2937dc6d
Aachal131 <83298583+Aachal131@users.noreply.github.com>
2024-01-28 20:14:59 +0530

    Add files via upload

--- SnakeLadderProblem.java
+++ SnakeLadderProblem.java
@@ -1,7 +1,48 @@
 package com.pratice;
 
+import java.util.Random;
+
 public class SnakeLadderProblem {
     public static void main (String args[]){
+        int startPosition =0;
+        int diceRolls = 0;
         System.out.println("Welcome to SnakeLadderProblem");
+        Random random = new Random();
+
+        while (startPosition  < 100) {
+            int diceValue = random.nextInt(6) + 1;
+            int option = random.nextInt(3);
+
+            switch (option) {
+                case 0:
+                    System.out.println("No Play");
+                    break;
+                case 1:
+                    System.out.println("Ladder!");
+                    startPosition  += diceValue;
+                    break;
+                case 2:
+                    System.out.println("Snake!");
+                    startPosition  -= diceValue;
+                    if (startPosition  < 0) {
+                        startPosition  = 0;
+                    }
+                    break;
+            }
+            diceRolls++;
+            System.out.println("Dice value: " + diceValue);
+            System.out.println("Current position: " + startPosition );
+            System.out.println("--------------------");
+            if (startPosition < 0) {
+                startPosition = 0;
+            } else if (startPosition > 100) {
+                startPosition = 100;
+            }
+
+
+        }
+
+        System.out.println("reaches the winning position 100!");
+        System.out.println("Number of dice rolls to win: " + diceRolls);
     }
 }
